320ff4249898539bace2559080895d2cf5f49db3
s890500-dev <s890500@gmail.com>
2026-02-05 14:12:34 +0800

    Install required dependencies and create vault module exports

---
+++ src/modules/vault/index.ts
@@ -0,0 +1,7 @@
+// Public API exports for vault module
+export * from './types/vault.types';
+export * from './components/vault-card';
+export * from './components/empty-state';
+export * from './stores/vault.store';
+export * from './constants/mock-pools';
+
