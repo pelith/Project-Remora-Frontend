5d1691a90acb3f51e2571ad380ed7f9337b6f3b8
s890500-dev <s890500@gmail.com>
2026-02-05 14:35:06 +0800

    Add Toaster import to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
+import { Toaster } from 'sonner';
 
 import Header from '../components/Header';
 
